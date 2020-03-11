61b2d59a27a02eb6274cbf8007be935b6f1c29eb
Manuel <sufferdarkness44@gmmail.com>
2020-03-11 16:53:58 -0600

    'progress_bars'

---
+++ progress.js
@@ -0,0 +1,8 @@
+function animar()
+{
+	document.getElementById('hola').classList.toggle("final");
+	document.getElementById('hola2').classList.toggle("final");
+	document.getElementById('hola3').classList.toggle("final");
+	document.getElementById('hola4').classList.toggle("final");
+	document.getElementById('hola5').classList.toggle("final");
+}
